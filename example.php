b047266eb2ca7f6728eed81162b4e2854b07200b
Brian Huisman <bhuisman@greywyvern.com>
2023-04-12 08:13:09 -0400

    Update directory name to orcinus

--- example.php
+++ example.php
@@ -1,13 +1,13 @@
 <?php
 
-require 'os3/search.php';
+require 'orcinus/search.php';
 
 ?><!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="os3/css/search.css">
+  <link rel="stylesheet" href="orcinus/css/search.css">
   <title>Orca PHP Search <?php echo $_ODATA['version']; ?></title>
 </head>
 <body>
