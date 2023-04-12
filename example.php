97bf09dd86704d0532822170a8b85559271e9e25
Brian Huisman <bhuisman@greywyvern.com>
2023-04-12 19:12:51 -0400

    Update examples with typeahead

--- example.php
+++ example.php
@@ -1,18 +1,24 @@
 <?php
 
-require 'orcinus/search.php';
+require 'search.php';
 
 ?><!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="orcinus/css/search.css">
-  <title>Orca PHP Search <?php echo $_ODATA['version']; ?></title>
+  <title>Orcinus Site Search - Online example</title>
+
+  <link rel="stylesheet" href="css/search.css">
 </head>
 <body>
-  <h1>Orca PHP Search <?php echo $_ODATA['version']; ?></h1>
+  <h1>Orcinus Site Search - Online example</h1>
 
   <?php $_TEMPLATE->render(); ?> 
+
+  <!-- Script files below are only required for Typeahead -->
+  <script src="js/jquery-3.6.4.min.js"></script>
+  <script src="js/typeahead.bundle.min.js"></script>
+  <script src="js/search.js"></script>
 </body>
 </html>
